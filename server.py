124f05cddce8098cd32a93449da33aa53c437a80
Igor <f0resight@list.ru>
2021-03-14 21:24:26 +0300

    Update

--- server.py
+++ server.py
@@ -5,11 +5,16 @@ PORT = 8080
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.path = 'base.html'
+        self.path = 'index.html'
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
 Handler = MyHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("Start server at 127.0.0.1:", PORT)
-    httpd.serve_forever()
+    print("Run server at 127.0.0.1:{}".format(PORT))
+    
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print('catched!')
+
